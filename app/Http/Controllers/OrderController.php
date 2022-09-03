5cb837abaee4ec9e1a555c2f24e33d2e355026a1
Shoaib Arshad <shoaibarshad6578303@gmail.com>
2022-09-03 21:03:37 +0500

    repository pattern

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use App\Http\Requests\StoreOrderRequest;
+use App\Http\Requests\UpdateOrderRequest;
+use App\Interfaces\OrderRepositoryInterface;
+use Illuminate\Http\JsonResponse;
+
+class OrderController extends Controller
+{
+    private OrderRepositoryInterface $orderRepository;
+
+    public function __construct(OrderRepositoryInterface $orderRepository) 
+    {
+        $this->orderRepository = $orderRepository;
+    }
+
+    public function index(): JsonResponse 
+    {
+        return response()->json([
+            'data' => $this->orderRepository->getAllOrders()
+        ]);
+    }
+
+    public function store(Request $request): JsonResponse 
+    {
+        $orderDetails = $request->only([
+            'client',
+            'details'
+        ]);
+
+        return response()->json(
+            [
+                'data' => $this->orderRepository->createOrder($orderDetails)
+            ],
+            Response::HTTP_CREATED
+        );
+    }
+
+    public function show(Request $request): JsonResponse 
+    {
+        $orderId = $request->route('id');
+
+        return response()->json([
+            'data' => $this->orderRepository->getOrderById($orderId)
+        ]);
+    }
+
+    public function update(Request $request): JsonResponse 
+    {
+        $orderId = $request->route('id');
+        $orderDetails = $request->only([
+            'client',
+            'details'
+        ]);
+
+        return response()->json([
+            'data' => $this->orderRepository->updateOrder($orderId, $orderDetails)
+        ]);
+    }
+
+    public function destroy(Request $request): JsonResponse 
+    {
+        $orderId = $request->route('id');
+        $this->orderRepository->deleteOrder($orderId);
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
+}
